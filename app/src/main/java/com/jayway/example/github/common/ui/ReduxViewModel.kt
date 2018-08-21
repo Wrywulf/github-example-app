69754d96849864aeee84e4ff846978eef505b518
Ulrik Bech Hald <ulrik.hald@jayway.com>
2018-08-21 10:43:41 +0200

    updated documentation on ReduxViewModel

--- app/src/main/java/com/jayway/example/github/common/ui/ReduxViewModel.kt
+++ app/src/main/java/com/jayway/example/github/common/ui/ReduxViewModel.kt
@@ -12,13 +12,41 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.plusAssign
 import timber.log.Timber
 
+/**
+ * An abstraction over [ViewModel] that enables RxRedux behaviour.
+ *
+ * Configuration and subscription to streams is handled completely transparent to the subclass, which
+ * basically just have to declare: [Action] and [State] types as well as implement the list of [SideEffect]s
+ * and the state reducer.
+ *
+ */
 abstract class ReduxViewModel<Action, State, Arguments> : ViewModel(),
                                                           BindableViewModel<Screen<Action, State>, Arguments> {
 
+    /**
+     * The initial state of the [ViewModel], after instantiation.
+     */
     protected abstract val initialState: State
 
+    /**
+     * The list of side effects involved in the manipulation of state for this view model. An example
+     * of a side effect could be fetching data from a external store.
+     *
+     * Side effects really model the asynchronous tasks fired off within the state machine upon receipt
+     * of an [Action]. Side effects do not change [State] directly, but they may output new [Action]s
+     * that will.
+     *
+     * Another term for side-effect is "Use case".
+     */
     protected abstract val sideEffects: List<SideEffect<State, Action>>
 
+    /**
+     * The core of the state machine in the view model. It receives every [Action] input to the view
+     * model as well as internal [Action]s fired from the side effects.
+     *
+     * Based on current state and a received action, it outputs (synchronously) a state. The output
+     * state could be the same state as before, or a new state.
+     */
     protected abstract fun reducer(state: State, action: Action): State
 
     private val actions = PublishRelay.create<Action>()
@@ -26,14 +54,14 @@ abstract class ReduxViewModel<Action, State, Arguments> : ViewModel(),
     private val stateMachine: ObservableTransformer<Action, State> =
         ObservableTransformer { upstream ->
             upstream.doOnNext {
-                Timber.d("UserAction input: $it")
+                Timber.v("UserAction input: $it")
             }
                 .reduxStore(
                     initialState = initialState, sideEffects = sideEffects, reducer = ::reducer
                 )
                 .distinctUntilChanged()
                 .doOnNext {
-                    Timber.d("State output: $it")
+                    Timber.v("State output: $it")
                 }
         }
 
